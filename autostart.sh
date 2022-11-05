9b7e80e0b5946ebbac934f0bc457e4e2eb78b284
Kiril Urivsky <kirilurivsky@gmail.com>
2022-11-05 08:38:30 +0200

    cleaning

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 sudo ufw enable
-qbittorrent --save-path=/home/kiril/torrents/JPS --category="JPS"
+qbittorrent --save-path=$HOME/torrents/JPS --category="JPS"
 nicotine --enable-trayicon
-redis-server; cd /home/kiril/teddit/; npm start
 sudo tailscale up
 sudo systemctl start lighttpd; pihole -up && pihole -g
-export ND_PLAYLISTSPATH=/home/kiril/Music/.playlists && sudo systemctl start navidrome
+export ND_PLAYLISTSPATH=$HOME/Music/.playlists && sudo systemctl start navidrome
 # sudo -H -u searx -i && python /usr/local/searx/searx-src/searx/webapp.py
-
+redis-server; cd $HOME/teddit/; npm start
 
