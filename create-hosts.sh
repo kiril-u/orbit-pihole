f8f3bf1869eabf6807a3d098999753d051c5bae0
Kiril <kirilurivsky.isr@gmail.com>
2023-02-19 04:15:58 +0200

    Working on some big changes
    Signed-off-by: kiril-u <kirilurivsky@gmail.com>

--- create-hosts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f ./hosts ]; then
-    mv hosts hosts.old
-fi
-cat kirilhosts.0 kirilhosts.1 kirilhosts.2 kirilhosts.3 kirilhosts.4 kirilhosts.5 kirilhosts.6 kirilhosts.7 kirilhosts.8 kirilhosts.9 > hosts.temp
-awk '!seen[$0]++' hosts.temp > hosts
-# perl -i -ne 'print if ! $x{$_}++' hosts.temp2 > hosts
-rm hosts.temp
